f17d4ac3a63d57ddd251e7b24dfa0bd8a4984d5e
Jessa Halversen <jessalethbridge@gmail.com>
2024-04-09 18:10:30 -0600

    Can't get seaborn to load in

--- Hamilton_streamlit.py
+++ Hamilton_streamlit.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 st.title("Lewis Hamilton F1 Race Stats")
 
@@ -21,5 +23,14 @@ with tab1:
     plt.legend(loc = (1.01, 0))
     plt.subplots_adjust(right=0.8);  # Increase the right margin to make room for the legend
 
-    plt.savefig("Hamilton_races.png")
 
+with tab2:
+    mclar_data = ham_df[ham_df['Team'] == "McLaren"]
+    sns.lineplot(data = mclar_data, x = "Date", y = 'Position', palette = 'orange')
+
+    # Flip the y-axis so higher positions are at the top 
+    plt.gca().invert_yaxis()
+    plt.ylim(1, 25)
+    plt.title("Hamilton's Races over the Years")
+    plt.legend(loc = (1.01, 0))
+    plt.subplots_adjust(right=0.8);  # Increase the right margin to make room for the legend
