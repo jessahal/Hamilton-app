9c92d4b1c9f107727e22f6869f7260f2134f75b0
Jessa Halversen <jessalethbridge@gmail.com>
2024-04-09 18:14:16 -0600

    Added some stuff

--- Hamilton_streamlit.py
+++ Hamilton_streamlit.py
@@ -14,23 +14,26 @@ tab1, tab2 = st.tabs(['Mercedes', 'McLaren'])
 
 with tab1:
     merc_data = ham_df[ham_df['Team'] == "Mercedes"]
-    sns.lineplot(data = merc_data, x = "Date", y = 'Position', palette = 'turquoise')
+    merc_fig = sns.lineplot(data = merc_data, x = "Date", y = 'Position', palette = 'turquoise')
 
     # Flip the y-axis so higher positions are at the top 
-    plt.gca().invert_yaxis()
-    plt.ylim(1, 25)
-    plt.title("Hamilton's Races over the Years")
-    plt.legend(loc = (1.01, 0))
-    plt.subplots_adjust(right=0.8);  # Increase the right margin to make room for the legend
-
+    merc_fig.gca().invert_yaxis()
+    merc_fig.ylim(1, 25)
+    merc_fig.title("Hamilton's Races over the Years")
+    merc_fig.legend(loc = (1.01, 0))
+    merc_fig.subplots_adjust(right=0.8);  # Increase the right margin to make room for the legend
+    st.header(f'Team {tab1}')
+    st.plotly_chart(merc_fig)
 
 with tab2:
     mclar_data = ham_df[ham_df['Team'] == "McLaren"]
-    sns.lineplot(data = mclar_data, x = "Date", y = 'Position', palette = 'orange')
+    mclar_fig = sns.lineplot(data = mclar_data, x = "Date", y = 'Position', palette = 'orange')
 
     # Flip the y-axis so higher positions are at the top 
-    plt.gca().invert_yaxis()
-    plt.ylim(1, 25)
-    plt.title("Hamilton's Races over the Years")
-    plt.legend(loc = (1.01, 0))
-    plt.subplots_adjust(right=0.8);  # Increase the right margin to make room for the legend
+    mclar_fig.gca().invert_yaxis()
+    mclar_fig.ylim(1, 25)
+    mclar_fig.title("Hamilton's Races over the Years")
+    mclar_fig.legend(loc = (1.01, 0))
+    mclar_fig.subplots_adjust(right=0.8);  # Increase the right margin to make room for the legend
+    st.header(f'Team {tab2}')
+    st.plotly_chart(mclar_fig)
